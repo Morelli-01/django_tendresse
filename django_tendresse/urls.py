88dea0aca2fa83ce3d630fb11c8e7ddd572d6a66
nicola <nicolamorelli30008@gmail.com>
2025-09-09 13:13:04 +0200

    added favicon redirect

--- django_tendresse/urls.py
+++ django_tendresse/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from django.views.static import serve
 from django.views.generic.base import TemplateView
 
@@ -30,6 +31,8 @@ urlpatterns = [
     path("sitemap.xml",TemplateView.as_view(template_name="seo/sitemap.xml", content_type="application/xml")),
     path("humans.txt",TemplateView.as_view(template_name="seo/humans.txt", content_type="text/plain")),
     path("llms.txt",TemplateView.as_view(template_name="seo/llms.txt", content_type="text/plain")),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico')),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
